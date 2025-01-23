5665eaa2e30c3d2f155fc09a82cfef06e60a2298
BerinMadzak <berinmadzak@gmail.com>
2025-01-23 15:50:01 +0100

    Add cart icon

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,10 @@
 export default function Header() {
     return (
         <div className="header">
-            
+            <div className="cart-container">
+                <i className="cart-icon fa-solid fa-cart-shopping"></i>
+                <p className="cart-count">0</p>
+            </div>
         </div>
     );
 }

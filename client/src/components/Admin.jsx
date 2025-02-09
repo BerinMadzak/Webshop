1f2da6d8a6986c54df1a1483591163162f15c26f
BerinMadzak <berinmadzak@gmail.com>
2025-02-09 13:08:32 +0100

    Add UI for admin page

--- client/src/components/Admin.jsx
+++ client/src/components/Admin.jsx
@@ -1,7 +1,15 @@
 export default function Admin() {
     return (
         <div>
-            <h1>WIP</h1>
+            <h1>Admin Dashboard</h1>
+            <div className="admin-grid">
+                <button>Add Product</button>
+                <button>Update Product</button>
+                <button>Delete Product</button>
+                <button>Create Discount</button>
+                <button>Delete Discount</button>
+                <button>View All Discounts</button>
+            </div>
         </div>
     );
 }
